9cda42f7d69614d2f3795eb19a8ae62e5133b64e
Willfoss <118803774+Willfoss@users.noreply.github.com>
2023-11-11 11:32:10 +0000

    created three buttons for RPS and added event listener to buttons. result outputed after each round.

--- script.js
+++ script.js
@@ -3,48 +3,57 @@ function getComputerChoice(){
     return arr[Math.floor(Math.random()*3)];
 }
 
+
+
 function oneRound(playerSelection, computerSelection){
+    let resultDiv = document.querySelector("#results");
     let playerChoice = playerSelection.toLowerCase();
     let computerChoice = computerSelection.toLowerCase();
+    resultDiv.textContent="";
     if (playerChoice === computerChoice ){
-        return "draw";
+        resultDiv.textContent = "draw";
     } else if (playerChoice == "rock" && computerChoice == "paper") {
-        return "You Lose! Paper beats Rock";
+        resultDiv.textContent = "You Lose! Paper beats Rock";
     } else if (playerChoice == "rock" && computerChoice == "scissors"){
-        return "You Win! Rock beats Scissors";
+        resultDiv.textContent = "You Win! Rock beats Scissors";
     } else if (playerChoice == "paper" && computerChoice == "rock") {
-        return "You Win! Paper beats Rock";
+        resultDiv.textContent =  "You Win! Paper beats Rock";
     } else if (playerChoice == "paper" && computerChoice == "scissors"){
-        return "You Lose! Scissors beat Paper"
+        resultDiv.textContent =  "You Lose! Scissors beat Paper"
     }  else if (playerChoice == "scissors" && computerChoice == "paper") {
-        return "You Win! Scissors beat Paper";
+        resultDiv.textContent =  "You Win! Scissors beat Paper";
     } else if (playerChoice == "scissors" && computerChoice == "rock"){
-        return "You Lose! Rock beats Scissors";
+        resultDiv.textContent =  "You Lose! Rock beats Scissors";
     }
+    return resultDiv.textContent;
 }
+
+let choice = document.querySelector("#rps-selection");
+
+choice.addEventListener("click", (event)=> {
+    let rockPaperScissorChoice = event.target;
+
+    switch(rockPaperScissorChoice.id){
+        case "rock":
+            console.log("rock");
+            oneRound("rock", getComputerChoice());
+            break;
+        case "paper":
+            console.log("paper");
+            oneRound("paper", getComputerChoice());
+            break;
+        case "scissors":
+            console.log("scissors");
+            oneRound("scissors", getComputerChoice());
+            break;
+    }
+});
+
+
+
 
 function game(){
-    let playerScore=0;
-    let computerScore=0;
-    for (let i=1; i<=5; i++){
-        let playerSelection = prompt("pick Rock, Paper or Scissors", "rock");
-        let winner = oneRound(playerSelection, getComputerChoice());
-        console.log(winner);
-        if (winner.slice(0, 8)=="You Win!"){
-            playerScore+=1;
-        } else if (winner.slice(0, 9)=="You Lose!") {
-            computerScore+=1;
-        } else {
-            playerScore=playerScore;
-            computerScore=computerScore;
-            i--;
-        }
-        console.log(playerScore);
-        console.log(computerScore);
-    }
-
 }
-game();
 
 //const playerSelection = "rock";
 //const computerSelection = getComputerChoice();
